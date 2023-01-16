aa3b3521e4e95b4e306254dcc4227f479462b047
AswathiMohan23 <achu999here@gmail.com>
2023-01-16 21:16:34 +0530

    [ASWATHI] added . code to delete the details of a person from the list using first name

--- AddressBook/address_book.py
+++ AddressBook/address_book.py
@@ -1,18 +1,17 @@
-print("\n\t\t\t\t\t\t\t\t WELCOME TO ADDRESSBOOK \n\t\t\t\t\t\t\t  ***************************\n")
+print("\n\t\t\t\t\t\t\t\t\t WELCOME TO ADDRESSBOOK \n\t\t\t\t\t\t\t\t  ***************************\n")
 
 
 def adding_data_from_console():
+    print("\n------------------------------------------------------ UC2 ----------------------------------------------------------------------\n")
     data = adding_details()
     return data
 
 
-def display_contactBook():
+def display_contactBook(contact):
     print("\t\t\t\t\t---------------- Existing contact book ------------------------ \n\n", contact)
 
 
 def adding_details():
-    print("------------------------------------------------------ UC2 ----------------------------------------------------------------------\n")
-
     first_name = input("Enter the first name : ")
     second_name = input("Enter the first name : ")
     address = input("Enter the address : ")
@@ -27,21 +26,44 @@ def adding_details():
 
 
 def edit_data():
-    print("------------------------------------------------------ UC3 ----------------------------------------------------------------------\n")
-
+    print("\n------------------------------------------------------ UC3 ----------------------------------------------------------------------\n")
     name_to_be_edited = input("enter the first name to edit the contact : ")
     if name_to_be_edited == person1.get('first_name'):
         data = adding_details()
         person1.update(data)
-        display_contactBook()
+        display_contactBook(contact)
     elif name_to_be_edited == person2.get('first_name'):
         data = adding_details()
         person2.update(data)
-        display_contactBook()
+        display_contactBook(contact)
+    elif name_to_be_edited == person3.get('first_name'):
+        data = adding_details()
+        person3.update(data)
+        display_contactBook(contact)
+
+
+def delete_contact():
+    print("\n------------------------------------------------------ UC4 ----------------------------------------------------------------------\n")
+    name_to_be_deleted = input("enter the first name of the person whose details should be deleted : ")
+    if name_to_be_deleted == person1.get('first_name'):
+        contact.remove(person1)
+        after_deletion(contact,name_to_be_deleted)
+    elif name_to_be_deleted == person2.get('first_name'):
+        contact.remove(person2)
+        after_deletion(contact,name_to_be_deleted)
+    elif name_to_be_deleted == person3.get('first_name'):
+        contact.remove(person3)
+        after_deletion(contact, name_to_be_deleted)
+
+
+def after_deletion(contact, name):
+    print("\n******************************* After deleting details of", name, " ******************************\n")
+    display_contactBook(contact)
+
 
 # ------------------------------------------------------------------------------------------------------------------
 
-
+print("------------------------------------------------------ UC1 ----------------------------------------------------------------------\n")
 contact = []
 person1 = {"first_name": "Tom", "second_name": "John", "address": "Abc Apartment", "city": "Bangalore",
                "state": "Karnataka", "zip_code": "123456", "phn": "91 9495123456", "email": "tom@gmail.com"}
@@ -49,7 +71,9 @@ person2 = {"first_name": "Ravi", "second_name": "Dev", "address": "pq villa", "c
                "zip_code": "120056", "phn": "91 9491122956", "email": "ravi@yahoo.com"}
 contact.append(person1)
 contact.append(person2)
-display_contactBook()
+display_contactBook(contact)
 person3 = adding_data_from_console()
-display_contactBook()
+contact.append(person3)
+display_contactBook(contact)
 edit_data()
+delete_contact()
