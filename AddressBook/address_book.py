f217dca32a647fc09994dd448aa966bef3bac1fb
AswathiMohan23 <achu999here@gmail.com>
2023-01-16 16:50:26 +0530

    [ASWATHI] added . code to create contact using dictionary and added 2 dictionaries to a list using append() to create a contactbook

--- AddressBook/address_book.py
+++ AddressBook/address_book.py
@@ -1,1 +1,7 @@
 print("\n-------------------------------- WELCOME TO ADDRESSBOOK --------------------------------\n")
+contact = []
+person1 = {"first_name": "Tom", "second_name": "John", "address": "Abc Apartment", "city": "Bangalore", "state": "Karnataka", "pin": "123456", "phn": "91 9495123456", "email": "tom@gmail.com"}
+person2 = {"first_name": "Ravi", "second_name": "Dev", "address": "pq villa", "city": "Kochi", "state": "Kerala", "pin": "120056", "phn": "91 9491122956", "email": "ravi@yahoo.com"}
+contact.append(person1)
+contact.append(person2)
+print(contact)
