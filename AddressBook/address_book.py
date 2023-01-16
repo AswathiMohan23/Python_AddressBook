922cdf2423ba7a1a0e016e6be274e844fc2ee5ae
AswathiMohan23 <achu999here@gmail.com>
2023-01-16 18:10:19 +0530

    [ASWATHI]  refactored . the code by editing the function to re-use the code

--- AddressBook/address_book.py
+++ AddressBook/address_book.py
@@ -1,17 +1,15 @@
 print("\n\t\t\t\t\t\t\t\t WELCOME TO ADDRESSBOOK \n\t\t\t\t\t\t\t  ***************************\n")
 
+def adding_data_from_console():
+    data = adding_details()
+    return data
 
-def existing_contacts():
-    person1.update({"first_name": "Tom", "second_name": "John", "address": "Abc Apartment", "city": "Bangalore",
-               "state": "Karnataka", "zip_code": "123456", "phn": "91 9495123456", "email": "tom@gmail.com"})
-    person2.update({"first_name": "Ravi", "second_name": "Dev", "address": "pq villa", "city": "Kochi", "state": "Kerala",
-               "zip_code": "120056", "phn": "91 9491122956", "email": "ravi@yahoo.com"})
-    contact.append(person1)
-    contact.append(person2)
+
+def display_contactBook():
     print("\t\t\t\t\t---------------- Existing contact book ------------------------ \n\n", contact)
 
 
-def adding_details_from_console():
+def adding_details():
     print("-------------------------------------------- UC2 ---------------------------------------------------\n")
 
     first_name = input("Enter the first name : ")
@@ -22,16 +20,28 @@ def adding_details_from_console():
     zip_code = input("Enter the zip_code : ")
     phn = input("Enter the phone number : ")
     email = input("Enter the email : ")
-    person3.update(
-        {"first_name": first_name, "second_name": second_name, "address": address, "city": city, "state": state,
-         "zip_code": zip_code, "phn": phn, "email": email})
-    print(person3)
+    data={"first_name": first_name, "second_name": second_name, "address": address, "city": city, "state": state,
+         "zip_code": zip_code, "phn": phn, "email": email}
+    return data
+
+
+"""def edit_data():
+    name_to_be_edited = input("enter the first name to edit the contact : ")
+    if person1.get('name_to_be_edited') == "Tom":
+        adding_details_from_console()"""
 
 
 # ------------------------------------------------------------------------------------------------------------------
+
 contact = []
-person1 = {}
-person2 = {}
-person3 = {}
-existing_contacts()
-adding_details_from_console()
+person1={"first_name": "Tom", "second_name": "John", "address": "Abc Apartment", "city": "Bangalore",
+               "state": "Karnataka", "zip_code": "123456", "phn": "91 9495123456", "email": "tom@gmail.com"}
+person2 = {"first_name": "Ravi", "second_name": "Dev", "address": "pq villa", "city": "Kochi", "state": "Kerala",
+               "zip_code": "120056", "phn": "91 9491122956", "email": "ravi@yahoo.com"}
+contact.append(person1)
+contact.append(person2)
+display_contactBook()
+person3 = adding_data_from_console()
+display_contactBook()
+
+#edit_data()
