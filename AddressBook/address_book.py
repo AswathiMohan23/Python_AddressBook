52c1a3c34ffceeda12464b143dc1640edfe2687f
AswathiMohan23 <achu999here@gmail.com>
2023-01-17 09:52:46 +0530

    [ASWATHI] refactored . the edit function to take multiple values

--- AddressBook/address_book.py
+++ AddressBook/address_book.py
@@ -1,11 +1,11 @@
 print("\n\t\t\t\t\t\t\t\t\t WELCOME TO ADDRESSBOOK \n\t\t\t\t\t\t\t\t  ***************************\n")
 
 
-def adding_data_from_console():
+def adding_data_from_console(contact):
     print("\n------------------------------------------ UC5 (refactored UC2 to add multiple contacts) ----------------------------------------------------------------------\n")
-    limit = int(input("Enter the no of contacts you eed to add to the contact book : "))
-    for i in range (1,limit+1):
-        print("\nenter the details of contact ", i)
+    limit = int(input("Enter the no of contacts you need to add to the contact book : "))
+    for i in range (0,limit):
+        print("\nenter the details of contact ", i+1)
         data = adding_details()
         contact.append(data)
 
@@ -16,7 +16,7 @@ def display_contactBook(contact):
 
 def adding_details():
     first_name = input("Enter the first name : ")
-    second_name = input("Enter the first name : ")
+    second_name = input("Enter the second name : ")
     address = input("Enter the address : ")
     city = input("Enter the city : ")
     state = input("Enter the state : ")
@@ -28,18 +28,15 @@ def adding_details():
     return data
 
 
-def edit_data():
+def edit_data(contact):
     print("\n------------------------------------------------------ UC3 ----------------------------------------------------------------------\n")
     name_to_be_edited = input("enter the first name to edit the contact : ")
-    if name_to_be_edited == person1.get('first_name'):
-        data = adding_details()
-        person1.update(data)
-        display_contactBook(contact)
-    elif name_to_be_edited == person2.get('first_name'):
-        data = adding_details()
-        person2.update(data)
-        display_contactBook(contact)
-
+    for dict in contact:
+        for lst in dict:
+            if dict.get(lst) == name_to_be_edited:
+                data = adding_details()
+                dict.update(data)
+                return dict
 
 
 def delete_contact():
@@ -53,11 +50,24 @@ def delete_contact():
         after_deletion(contact,name_to_be_deleted)
 
 
-
 def after_deletion(contact, name):
     print("\n******************************* After deleting details of", name, " ******************************\n")
     display_contactBook(contact)
 
+
+"""def get_value(contact, key):
+    result = [i[key] for i in contact]
+    if result == 'Tom':
+        data=adding_details()
+        contact.index()
+    return result"""
+
+
+def get_value(lst, key, value):
+    for i, contact in enumerate(lst):
+        if contact[key] == value:
+            data = adding_details();
+            return data
 
 
 # ------------------------------------------------------------------------------------------------------------------
@@ -71,7 +81,11 @@ person2 = {"first_name": "Ravi", "second_name": "Dev", "address": "pq villa", "c
 contact.append(person1)
 contact.append(person2)
 display_contactBook(contact)
-adding_data_from_console()
+
+adding_data_from_console(contact)
+
 display_contactBook(contact)
-edit_data()
+new_data=edit_data(contact)
+contact.append(new_data)
+display_contactBook(contact)
 delete_contact()
