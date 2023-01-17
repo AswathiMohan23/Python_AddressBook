e30abec6cfd977395c496248684d450ecc5ba33f
AswathiMohan23 <achu999here@gmail.com>
2023-01-17 08:02:21 +0530

    [ASWATHI] refactored  . code to add multiple contacts in the address book by refactoring UC2

--- AddressBook/address_book.py
+++ AddressBook/address_book.py
@@ -2,9 +2,12 @@ print("\n\t\t\t\t\t\t\t\t\t WELCOME TO ADDRESSBOOK \n\t\t\t\t\t\t\t\t  *********
 
 
 def adding_data_from_console():
-    print("\n------------------------------------------------------ UC2 ----------------------------------------------------------------------\n")
-    data = adding_details()
-    return data
+    print("\n------------------------------------------ UC5 (refactored UC2 to add multiple contacts) ----------------------------------------------------------------------\n")
+    limit = int(input("Enter the no of contacts you eed to add to the contact book : "))
+    for i in range (1,limit+1):
+        print("\nenter the details of contact ", i)
+        data = adding_details()
+        contact.append(data)
 
 
 def display_contactBook(contact):
@@ -36,10 +39,7 @@ def edit_data():
         data = adding_details()
         person2.update(data)
         display_contactBook(contact)
-    elif name_to_be_edited == person3.get('first_name'):
-        data = adding_details()
-        person3.update(data)
-        display_contactBook(contact)
+
 
 
 def delete_contact():
@@ -51,14 +51,13 @@ def delete_contact():
     elif name_to_be_deleted == person2.get('first_name'):
         contact.remove(person2)
         after_deletion(contact,name_to_be_deleted)
-    elif name_to_be_deleted == person3.get('first_name'):
-        contact.remove(person3)
-        after_deletion(contact, name_to_be_deleted)
+
 
 
 def after_deletion(contact, name):
     print("\n******************************* After deleting details of", name, " ******************************\n")
     display_contactBook(contact)
+
 
 
 # ------------------------------------------------------------------------------------------------------------------
@@ -72,8 +71,7 @@ person2 = {"first_name": "Ravi", "second_name": "Dev", "address": "pq villa", "c
 contact.append(person1)
 contact.append(person2)
 display_contactBook(contact)
-person3 = adding_data_from_console()
-contact.append(person3)
+adding_data_from_console()
 display_contactBook(contact)
 edit_data()
 delete_contact()
